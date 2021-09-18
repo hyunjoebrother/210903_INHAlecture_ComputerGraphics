80467cb1dadcf4f5df7a4eff3c2e526280e8cbe2
hyunjoebrother <hyunjoe31@gmail.com>
2021-09-18 23:41:27 +0900

    210918_Lab4_task

--- 210917_Lab4/main.cpp
+++ 210917_Lab4/main.cpp
@@ -30,7 +30,7 @@ void init(void)
 /* Callback 함수 정의 */
 
 // 점 그리기
-void draw_point(void)
+void draw_point()
 {
 	/* 점 색상 붉은색 */
 	glColor3f(1.0f, 0.0f, 0.0f);
@@ -39,6 +39,7 @@ void draw_point(void)
 
 	glBegin(GL_POINTS);
 	glVertex2i(pointX, pointY);
+	printf("point X, Y : (%d, %d)\n", pointX, pointY);
 	glEnd();
 }
 
@@ -175,33 +176,40 @@ void sub_menu_function(int option)
 {
 	printf("Submenu %d has been selected\n", option);
 
-	if (option == 1)
+	switch (option)
 	{
+	case 1 :
 		draw_point();
-	}
-	if (option == 2)
-	{
+		break;
+
+	case 2:
 		draw_line();
-	}
-	if (option == 3)
-	{
+		break;
+
+	case 3:
 		draw_triangle();
-	}
-	if (option == 4)
-	{
+		break;
+		
+	case 4:
 		draw_polygon();
-	}
-	if (option == 5)
-	{
-		clear();
+		break;
 	}
 }
 
 void main_menu_function(int option)
 {
 	printf("Main menu %d has been selected\n", option);
-	if (option == 999) {
+
+	switch (option)
+	{
+	case 831:
+		printf("Lets Clear this window!\n");
+		clear();
+		break;
+
+	case 999:
 		exit(0);
+		break;
 	}
 }
 
@@ -232,11 +240,11 @@ int main(int argc, char** argv)
 	glutAddMenuEntry("Line", 2);
 	glutAddMenuEntry("Triangle", 3);
 	glutAddMenuEntry("Polygon", 4);
-	glutAddMenuEntry("Clear", 5);
 
 	glutCreateMenu(main_menu_function);
 
 	glutAddSubMenu("Wanna Draw? Choose Mode", submenu1);
+	glutAddMenuEntry("Clear", 831);
 	glutAddMenuEntry("Quit", 999);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
